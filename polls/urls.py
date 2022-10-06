851cb23262562e562bf64a6a7c2fa3da973c2a5f
rvboy <robertvboyle@gmail.com>
2022-10-06 18:08:03 -0400

    again v6

--- polls/urls.py
+++ polls/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('login', include('main.urls')),
 ]

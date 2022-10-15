b11fc1afbe504672d3b6ba71923c31bd8d4ebf01
Fishybonees <antjferg@gmail.com>
2022-10-15 14:30:57 -0400

    Fix ci

--- louslist/tests.py
+++ louslist/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 
-from .models import Question, User
 from django.test import Client
 
 def sign_in_test():

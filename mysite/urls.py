4b3c77f9f415e0a477669ae360aaeedbdca5304b
rvboy <robertvboyle@gmail.com>
2022-10-17 10:48:55 -0400

    google 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('louslist/', include('louslist.urls')),
     path('admin/', admin.site.urls),
-    path('login/', include('louslist.urls'))
+    #path('login/', include('louslist.urls'))
 ]

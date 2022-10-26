a845090cbbb7f9f4d1c5bc4b8c46cbf256799a6b
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2022-10-26 15:14:51 -0400

    Added more sorting

--- louslist/templatetags/lous_extra.py
+++ louslist/templatetags/lous_extra.py
@@ -14,13 +14,9 @@ def convertTime(value):
         return str(firstTime.strftime("%I:%M %p"))
     return value
 
-# 
 
 @register.filter(name='sortMeeting')
 def sortMeeting(value):
-    weekdays = ["MoWeFr","MoWe", "MoFr", "TuTh", "Mo", "Tu", "We", "Th", "Fr", "-"]
-    r = sorted(value, key=lambda valdict: ([-ord(c) for c in valdict["component"]], weekdays.index(valdict["meetings"][0]["days"]), valdict["meetings"][0]["start_time"] ))
-    print(r)
-
-    return r
+    weekdays = ["MoWeFr","MoWe", "MoFr", "TuTh", "WeFr", "Mo", "Tu", "We", "Th", "Fr", "-"]
+    return sorted(value, key=lambda valdict: ([-ord(c) for c in valdict["component"]], weekdays.index(valdict["meetings"][0]["days"]), valdict["meetings"][0]["start_time"]))
 

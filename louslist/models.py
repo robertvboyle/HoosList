3583afc441be9b89959979545e0cacb4747123a5
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2022-10-26 15:10:33 -0400

    Updated sorting by type, day, time

--- louslist/models.py
+++ louslist/models.py
@@ -14,15 +14,15 @@ class User(models.Model):
     email = models.CharField(max_length=30)
 
 
-class ClassModel(models.Model):
-    id = models.UUIDField(primary_key=True)
-    instructorName = models.CharField(max_length=100)
-    subject = models.CharField(max_length=8)
-    courseCode = models.IntegerField(max_length=5)
-    component = models.CharField(max_length=5)
-    waitList = models.IntegerField(max_length=3)
-    waitListCap = models.IntegerField(max_length=3)
-    classCap = models.IntegerField(max_length=3)
-    seatsOpen = models.IntegerField(max_length=3)
+# class ClassModel(models.Model):
+#     id = models.UUIDField(primary_key=True)
+#     instructorName = models.CharField(max_length=100)
+#     subject = models.CharField(max_length=8)
+#     courseCode = models.IntegerField(max_length=5)
+#     component = models.CharField(max_length=5)
+#     waitList = models.IntegerField(max_length=3)
+#     waitListCap = models.IntegerField(max_length=3)
+#     classCap = models.IntegerField(max_length=3)
+#     seatsOpen = models.IntegerField(max_length=3)
 
 
--- louslist/templatetags/lous_extra.py
+++ louslist/templatetags/lous_extra.py
@@ -14,3 +14,13 @@ def convertTime(value):
         return str(firstTime.strftime("%I:%M %p"))
     return value
 
+# 
+
+@register.filter(name='sortMeeting')
+def sortMeeting(value):
+    weekdays = ["MoWeFr","MoWe", "MoFr", "TuTh", "Mo", "Tu", "We", "Th", "Fr", "-"]
+    r = sorted(value, key=lambda valdict: ([-ord(c) for c in valdict["component"]], weekdays.index(valdict["meetings"][0]["days"]), valdict["meetings"][0]["start_time"] ))
+    print(r)
+
+    return r
+

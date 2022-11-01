55b13aa53fb46252875a6f64e6affa461d3202f1
Fishybonees <antjferg@gmail.com>
2022-11-01 14:35:26 -0400

    Small changes to model and start of profile page

--- louslist/models.py
+++ louslist/models.py
@@ -12,6 +12,34 @@ class User(models.Model):
     id_num = models.IntegerField()
     image = models.ImageField
     email = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=20)
+    friends = models.ManyToManyField('self', blank=True)
+    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class Schedule(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    courses = models.ManyToManyField('Course', blank=True)
+    def __str__(self):
+        return self.user.username
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    subject = models.CharField(max_length=20)
+    number = models.IntegerField()
+    section = models.IntegerField()
+    credits = models.IntegerField()
+    instructor = models.CharField(max_length=20)
+    days = models.CharField(max_length=20)
+    time = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    def __str__(self):
+        return self.course_id
+
 
 
 # class ClassModel(models.Model):
